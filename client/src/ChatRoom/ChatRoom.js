3e96da6e4e7af725f6f918057a58ae8c600c34c2
kelar crisp <kelcrisp007@icloud.com>
2020-07-08 11:48:18 -0700

    basic structure of chatRoom page

---
+++ client/src/ChatRoom/ChatRoom.js
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./ChatRoom.module.css";
+const ChatRoom = () => {
+  return <div className={classes.Container}></div>;
+};
+
+export default ChatRoom;
---
+++ client/src/ChatRoom/displayChat/DisplayChat.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./DisplayChat.module.css";
+const DisplayChat = () => {
+  return <div className={classes.Container}>DisplayChat</div>;
+};
+
+export default DisplayChat;
+DisplayChat;
